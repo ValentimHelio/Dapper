1f74ccdc5d930879eef4831175622842862d2a17
Valentim Helio de Oliveira <timvale@hotmail.com>
2023-08-14 10:26:33 -0400

    Adicionar arquivos de projeto.

---
+++ eCommerce.API/IRepositories/IUsuarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using eCommerce.API.Models;
+using System.Collections.Generic;
+
+namespace eCommerce.API.IRepositories
+{
+    public interface IUsuarioRepository
+    {
+        public List<Usuario> Get();
+        public Usuario GetById(int id);
+        public void Insert(Usuario usuario);
+        public void Update(Usuario usuario);
+        public void Delete(int id);
+    }
+}
---
+++ eCommerce.API/Repositories/UsuarioRepository.cs
@@ -0,0 +1,41 @@
+﻿using eCommerce.API.IRepositories;
+using eCommerce.API.Models;
+using System.Collections.Generic;
+
+namespace eCommerce.API.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private static List<Usuario> _db = new List<Usuario>()
+        {
+            new Usuario(){ Id=1, Nome="Filipe Rodrigues", Email="filipe.rodrigues@gmail.com" },
+            new Usuario(){ Id=2, Nome="Marcelo Rodrigues", Email="marcelo.rodrigues@gmail.com"},
+            new Usuario(){ Id=3, Nome="Jessica Rodrigues", Email="jessica.rodrigues@gmail.com"}
+        };
+
+        public List<Usuario> Get()
+        {
+            return _db;
+        }
+       
+        public Usuario GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Insert(Usuario usuario)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(Usuario usuario)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
